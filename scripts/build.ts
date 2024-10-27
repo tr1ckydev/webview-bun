72ccd2f6f5b7d651c40326955c0b50be6d87d271
citkane <michael@openpoint.ie>
2024-10-27 16:46:09 +0000

    Update build script and c64 artifact

--- scripts/build.ts
+++ scripts/build.ts
@@ -13,16 +13,18 @@ switch (platform) {
   case "linux":
     await $`
         cd webview
-        cmake -G Ninja -B build -S . -D CMAKE_BUILD_TYPE=Release -D WEBVIEW_WEBKITGTK_API=4.1 -D WEBVIEW_BUILD_AMALGAMATION=true -D WEBVIEW_PACKAGE_AMALGAMATION=true
+        export PATH=/usr/lib/llvm14/bin/:/usr/lib/llvm-14/bin/:/usr/lib64/llvm15/bin/:$PATH
+        cmake -G Ninja -B build -S . -D CMAKE_BUILD_TYPE=Release -D WEBVIEW_WEBKITGTK_API=6.0 -DWEBVIEW_ENABLE_CHECKS=false -DCMAKE_TOOLCHAIN_FILE=cmake/toolchains/host-llvm.cmake
         cmake --build build
         cp build/core/libwebview.so ../build/libwebview-${arch}.so
+        strip ../build/libwebview-${arch}.so
         `;
     break;
 
   case "darwin":
     await $`
         cd webview
-        cmake -G "Ninja Multi-Config" -B build -S . -DCMAKE_TOOLCHAIN_FILE=cmake/toolchains/universal-macos-llvm.cmake
+        cmake -G "Ninja Multi-Config" -B build -S . -DCMAKE_C_COMPILER=gcc-14 -DCMAKE_TOOLCHAIN_FILE=cmake/toolchains/universal-macos-llvm.cmake
         cmake --build build --config Release
         cp build/core/Release/libwebview.dylib ../build/libwebview.dylib
         strip -x -S ../build/libwebview.dylib
